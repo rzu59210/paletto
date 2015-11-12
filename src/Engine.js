3f5f13de0c07985de30e2a277fe9f75d961b0524
kevin wallaert <kevin wallaert>
2015-11-12 09:35:11 +0100

    Histoire1Modifier

--- src/Engine.js
+++ src/Engine.js
@@ -5,7 +5,6 @@ var Engine = function () {
 // private attributes and methods
     var plateau;
     var choisirCouleur;
-    var couleur = ['noir','vert','blanc','bleu','rouge','blanc'];
     var nbBille;
 // public methods
     this.initPlateau = function(){
@@ -13,20 +12,58 @@ var Engine = function () {
         plateau = new Array(6);
         for(var a=0; a<6;a++){
             plateau[a] = new Array(6);
-           // plateau[a] = couleur[a];
-            for(var b=0;b<6;b++){
-                plateau[a][b] = couleur[a];
-            }
         }
+        plateau[0][0] = "noir";
+        plateau[0][1] = "vert";
+        plateau[0][2] = "blanc";
+        plateau[0][3] = "bleu";
+        plateau[0][4] = "rouge";
+        plateau[0][5] = "blanc";
+
+        plateau[1][0] = "jaune";
+        plateau[1][1] = "blanc";
+        plateau[1][2] = "vert";
+        plateau[1][3] = "rouge";
+        plateau[1][4] = "jaune";
+        plateau[1][5] = "bleu";
+
+        plateau[2][0] = "bleu";
+        plateau[2][1] = "jaune";
+        plateau[2][2] = "bleu";
+        plateau[2][3] = "blanc";
+        plateau[2][4] = "noir";
+        plateau[2][5] = "rouge";
+
+        plateau[3][0] = "rouge";
+        plateau[3][1] = "noir";
+        plateau[3][2] = "rouge";
+        plateau[3][3] = "vert";
+        plateau[3][4] = "bleu"
+        plateau[3][5] = "blanc";
+
+
+        plateau[4][0] = "blanc";
+        plateau[4][1] = "vert";
+        plateau[4][2] = "jaune";
+        plateau[4][3] = "noir";
+        plateau[4][4] = "jaune";
+        plateau[4][5] = "vert";
+
+        plateau[5][0] = "jaune";
+        plateau[5][1] = "bleu";
+        plateau[5][2] = "noire";
+        plateau[5][3] = "rouge";
+        plateau[5][4] = "vert";
+        plateau[5][5] = "noir";
         console.log(plateau);
         return true;
     }
 
-    this.choisirCouleur = function(uneCouleur){
+   /* this.choisirCouleur = function(uneCouleur){
         couleur = uneCouleur;
         console.log("La couleur choisis :"+choisirCouleur);
         return uneCouleur;
-    }
+    }*/
 
     this.retirerBille = function(position){
         var x = position.charCodeAt(1) - 49;
